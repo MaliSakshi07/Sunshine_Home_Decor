0b5f37849d1852a5f4ce8cea14f0077923e2a547
MaliSakshi07 <sakshimali467@gmail.com>
2026-02-04 21:37:47 +0530

    Program

---
+++ C# Console/02_Odd_Even_Program/02_Odd_Even_Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Odd_Even_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int No = 3;
+
+            if(No % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+            Console.ReadKey();
+        }
+ 
+    }
+}

72d0c5dc71223be571555c968e539802380d2303
MaliSakshi07 <sakshimali467@gmail.com>
2026-02-05 21:25:55 +0530

    Program

---
+++ C# Console/03_Compairing_Two_Numbers/03_Compairing_Two_Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Compairing_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int No1 = 30, No2 = 32;
+
+            if(No1 > No2)
+            {
+                Console.WriteLine("No1 is big than No2 ");
+            }
+            else if(No1 < No2)
+            {
+                Console.WriteLine("No1 is smaller than No2");
+            }
+            else
+            {
+                Console.WriteLine("Both Numbers are Equal");
+            }
+            Console.ReadKey();
+        }
+    }
+}

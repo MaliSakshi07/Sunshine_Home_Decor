33cc4184031797e13a34e1753f55b3aa82c99aa2
MaliSakshi07 <sakshimali467@gmail.com>
2026-02-06 20:32:51 +0530

    Program

---
+++ C# Console/Numbers/Pallindrome_Number/Pallindrome_Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pallindrome_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dig = 0, temp = 0, pnum = 0;
+
+            Console.WriteLine("Enter Number :");
+            int No = Convert.ToInt32(Console.ReadLine());
+
+            temp = No;
+
+            while(No != 0)
+            {
+                dig = No % 10;
+                pnum = (pnum * 10) + dig;
+                No /= 10;
+            }
+            if(temp == pnum)
+            {
+                Console.WriteLine("Pallindrome Number");
+            }
+            else
+            {
+                Console.WriteLine("Not Pallindrome Number");
+            }
+            Console.ReadKey();
+        }
+    }
+}

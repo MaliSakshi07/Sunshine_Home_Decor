6506d019f7663dc60aef7c009171d1a202ee6eaf
MaliSakshi07 <sakshimali467@gmail.com>
2026-02-08 22:17:43 +0530

    Program

---
+++ C# Console/String/String_ Copy/String_ Copy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String__Copy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello";
+            string s2 = string.Copy(s1);
+
+            Console.WriteLine(s2);
+
+            Console.ReadKey();
+        }
+    }
+
+}

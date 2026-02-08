97bc4a5aa2cd19d1a66f3149b132a02842aab43d
MaliSakshi07 <sakshimali467@gmail.com>
2026-02-08 22:16:55 +0530

    Program

---
+++ C# Console/Abstract_Class/Abstract_Class/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    abstract class Employee
+    {
+        public string Name;
+        public abstract void calculateSalary();
+        public void Display()
+        {
+            Console.WriteLine("Employee Name :" + Name);
+        }
+    }
+
+    class Developer : Employee
+    {
+        public int BasicSalary = 20000;
+
+        public override void calculateSalary()
+        {
+            Console.WriteLine("Developer Salary :" + BasicSalary);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Developer();
+
+            emp.Name = "Sakshi Mali";
+            emp.Display();
+            emp.calculateSalary();
+        }
+    }
+}

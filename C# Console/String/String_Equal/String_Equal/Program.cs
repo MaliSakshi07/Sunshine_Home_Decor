abdb64307fe88b48b6a4113b220b8cd7dd6c51ec
MaliSakshi07 <sakshimali467@gmail.com>
2026-02-09 21:28:36 +0530

    Program

---
+++ C# Console/String/String_Equal/String_Equal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Equal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Sakshi";
+            string str2 = "Mali";
+            string str3 = "Sakshi";
+
+            Console.WriteLine("String Equal :" + str1.Equals(str2));
+            Console.WriteLine("\nString Equal :" + str1.Equals(str3));
+
+            Console.ReadKey();
+        }
+    }
+}

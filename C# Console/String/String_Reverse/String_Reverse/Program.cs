93e5b1e0ee41691be580a1cf85124cc953f0513d
MaliSakshi07 <sakshimali467@gmail.com>
2026-02-10 21:44:54 +0530

    Program

---
+++ C# Console/String/String_Reverse/String_Reverse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello";
+            string rev = "";
+
+            for(int i = str.Length - 1; i>=0; i--)
+            {
+                rev += str[i];
+            }
+
+            Console.WriteLine("Original String : " + str);
+            Console.WriteLine("Reverse String : " +rev);
+
+            Console.ReadKey();
+        }
+    }
+}

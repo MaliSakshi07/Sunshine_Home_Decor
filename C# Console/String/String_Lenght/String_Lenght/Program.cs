a25ef722b86cce2e475d766a930b5787375282c2
MaliSakshi07 <sakshimali467@gmail.com>
2026-02-10 21:47:07 +0530

    Program

---
+++ C# Console/String/String_Lenght/String_Lenght/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Lenght
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str = "Sakshi Mali";
+            int i = Str.Length;
+
+            Console.WriteLine("String Lenght :" + i);
+
+            Console.ReadKey();
+
+        }
+    }
+}
